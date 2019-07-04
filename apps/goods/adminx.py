1aee6957ecd9235a08943828e09782f414993101
fanyaow <yaowen_2008@qq.com>
2019-07-04 16:29:24 +0800

    修改xadmin商品列表的可修改字段

--- apps/goods/adminx.py
+++ apps/goods/adminx.py
@@ -6,43 +6,45 @@ from .models import IndexAd
 
 
 class GoodsAdmin(object):
-    #显示的列
+    # 显示的列
     list_display = ["name", "click_num", "sold_num", "fav_num", "goods_num", "market_price",
                     "shop_price", "goods_brief", "goods_desc", "is_new", "is_hot", "add_time"]
-    #可以搜索的字段
+    # 可以搜索的字段
     search_fields = ['name', ]
-    #列表页可以直接编辑的
+    # 列表页可以直接编辑的
     list_editable = ["is_hot", ]
-    #过滤器
+    # 过滤器
     list_filter = ["name", "click_num", "sold_num", "fav_num", "goods_num", "market_price",
                    "shop_price", "is_new", "is_hot", "add_time", "category__name"]
-    #富文本编辑器
+    # 富文本编辑器
     style_fields = {"goods_desc": "ueditor"}
-    list_editable = ['is_hot','shop_price','click_num','fav_num','goods_num']
-    #在添加商品的时候可以添加商品图片
+    list_editable = ['is_hot', 'shop_price', 'click_num', 'fav_num', 'goods_num']
+
+    # 在添加商品的时候可以添加商品图片
     class GoodsImagesInline(object):
         model = GoodsImage
         exclude = ["add_time"]
         extra = 1
         style = 'tab'
+
     inlines = [GoodsImagesInline]
 
 
 class GoodsCategoryAdmin(object):
-    list_display = ["name", "category_type", "parent_category",'is_tab', "add_time"]
+    list_display = ["name", "category_type", "parent_category", 'is_tab', "add_time"]
     list_filter = ["category_type", "parent_category", "name"]
     search_fields = ['name', ]
 
 
-
 class GoodsBrandAdmin(object):
     list_display = ["category", "image", "name", "desc"]
+
     # 重载默认get_context方法
     def get_context(self):
         context = super(GoodsBrandAdmin, self).get_context()
         # if 'form' in context固定写法
         if 'form' in context:
-            #fields['category']，只取这个外键（分类），  category_type=1只取分类的大类
+            # fields['category']，只取这个外键（分类），  category_type=1只取分类的大类
             context['form'].fields['category'].queryset = GoodsCategory.objects.filter(category_type=1)
         return context
 
@@ -66,4 +68,3 @@ xadmin.site.register(GoodsCategoryBrand, GoodsBrandAdmin)
 
 xadmin.site.register(HotSearchWords, HotSearchAdmin)
 xadmin.site.register(IndexAd, IndexAdAdmin)
-
